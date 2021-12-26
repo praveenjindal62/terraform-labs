2bafd8ead1f140bcbda69e852b2db092d72e4000
Praveen Jindal <praveenjindal62@gmail.com>
2021-12-26 23:25:33 +0530

    Updated variables.tf with defaults

--- azure-static-website-storage-explorer/variables.tf
+++ azure-static-website-storage-explorer/variables.tf
@@ -1,16 +1,21 @@
 variable "client_id" {
   type = string
+  default = ""
 }
 variable "client_secret" {
   type = string
+  default = ""
 }
 variable "tenant_id" {
   type = string
+  default = ""
 }
 variable "subscription_id" {
   type = string
+  default = ""
 }
 
 variable "storage_key" {
   type = string
+  default = ""
 }
