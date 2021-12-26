e813a3303f816afc2e773e6720c3d7569ccc812c
Praveen Jindal <praveenjindal62@gmail.com>
2021-12-26 23:52:57 +0530

    Added output variable for storageName

--- azure-static-website-storage-explorer/output.tf
+++ azure-static-website-storage-explorer/output.tf
@@ -1,3 +1,7 @@
 output "https-endpoint" {
     value = azurerm_storage_account.storage.primary_web_endpoint
 }
+
+output "storage-account-name" {
+    value = azurerm_storage_account.storage.name
+}
